ac0e949c7412ea9f86be357c83b0e831b1f47f2c
aadrae <aadrae94@gmail.com>
2023-04-06 16:08:34 -0700

    nested loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,30 +6,32 @@
 
 void times_table(void)
 {
-	int i, j, n;
-	char c;
+	int i, j, k;
 
-	c = ',';
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			n = i * j;
-			if (n < 10)
+			k = j * i;
+			if (j == 0)
 			{
-				_putchar (n + '0');
-				_putchar (c);
-				_putchar (' ');
-				_putchar (' ');
+				_putchar(k + '0');
 			}
-			else
+
+			if (k < 10 && j != 0)
 			{
-				_putchar ((n / 10) + '0');
-				_putchar ((n % 10) + '0');
-				_putchar (c);
-				_putchar (' ');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
-		_putchar ('\n');
+		_putchar('\n');
 	}
 }
