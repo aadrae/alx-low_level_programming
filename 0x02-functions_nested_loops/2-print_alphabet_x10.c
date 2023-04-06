3a7b249a05f886157c63882ba91a923c5b65063e
aadrae <aadrae94@gmail.com>
2023-04-06 09:31:51 -0700

    nested-loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- *print_alphabet_10_times-in_lowcase
+ *print_alphabet_x1-0print_alphabet_10_times-in_lowcase
  */
 
 void print_alphabet_x10(void)
 {
-	char c;
-	int a;
+	char a;
+	int c;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (c = 0; c <= 9; c++)
 	{
-		for (a = 0; a <= 9; a++)
-			_putchar(c);
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
 	}
 	_putchar('\n');
 }
