c5fda932f036906306359228668fe24ea6e40c3e
aadrae <aadrae94@gmail.com>
2023-04-06 16:24:25 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *add - function adds two integer
+ * @n: int to be added to b
+ * @m: int to be added to a
+ * Return: sum of n and m
+
+ */
+
+int add(int n, int m)
+{
+	int s;
+
+	s = n + m;
+	return (s);
+}
