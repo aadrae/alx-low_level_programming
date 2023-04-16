90aa1b998fdace8377283530e15b01d9aab7f203
aadrae <aadrae94@gmail.com>
2023-04-16 07:45:52 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ * print_triangle - entry point
+ *@size: size of the triangle
+ * Return: void
+ */
+
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = size - a; b >= 1; b--)
+			{
+				_putchar(' ');
+			}
+			for (c = 1; c <= a; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
