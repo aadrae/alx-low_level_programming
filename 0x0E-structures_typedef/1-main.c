b517622b49e4219a190253f72f874e6da507f3bf
aadrae <aadrae94@gmail.com>
2023-04-17 19:22:47 -0700

    Structures, typedef

---
+++ 0x0E-structures_typedef/1-main.c
@@ -0,0 +1,22 @@
+. A dog is the only thing on earth that loves you more than you love yourself
+mandatory
+Write a function that initialize a variable of type struct dog
+
+Prototype: void init_dog(struct dog *d, char *name, float age, char *owner);
+julien@ubuntu:~/0x0d. structures, typedef$ cat 1-main.c
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    struct dog my_dog;
+
+    init_dog(&my_dog, "Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    return (0);
+}
